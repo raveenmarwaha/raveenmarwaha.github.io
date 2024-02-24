cc32188363c60355e0dd75283beb80ce33843c95
raveenmarwaha <raveen.marwaha@torontomu.ca>
2024-02-24 18:59:43 -0500

    .

--- script.js
+++ script.js
@@ -9,19 +9,16 @@ function promptForName() {
     }
 }
 
-
-// Function to enlarge the clicked image
 function enlargeImage() {
     var image = document.getElementById("profile-image");
-    image.style.width = "500px"; // Change this value to your desired enlargement
-}
-// c. Customized Navigation Menu
-function navigateToPage(page) {
-    alert("Navigating to " + page);
-    // You can implement actual navigation logic here
+    image.style.width = "500px";
 }
 
-// d. Onmouseover/Onmouseout Event for Nav Items
+function navigateToPage(page) {
+    alert("Navigating to " + page);
+    
+}
+
 function changeColorOnHover(element, color) {
     element.style.backgroundColor = color;
 }
